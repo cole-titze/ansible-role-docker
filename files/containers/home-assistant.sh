2aa4f3159a4a8c6c0eb4285c15d65f2c60a092c4
Cole Titze <coletitze@MacBookPro.lan>
2025-04-24 20:34:41 -0500

    Change image of home assistant

--- files/containers/home-assistant.sh
+++ files/containers/home-assistant.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker stop -t 600 home-assistant
 docker rm home-assistant
-docker pull homeassistant/raspberrypi4-homeassistant:latest
+docker pull ghcr.io/home-assistant/home-assistant:stable
 docker run -d --name home-assistant --restart=always --device /dev/ttyUSB0:/dev/ttyUSB0 -v /var/homeassistant:/config --network host homeassistant/raspberrypi4-homeassistant
