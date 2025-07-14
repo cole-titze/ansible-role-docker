99443d37f1ecaf616971d9810bfee97d9679c9ef
Cole Titze <>
2025-07-13 22:00:39 -0500

    Update portainer

--- files/containers/portainer.sh
+++ files/containers/portainer.sh
@@ -2,4 +2,15 @@
 docker stop portainer
 docker rm portainer
 docker pull portainer/portainer-ce:latest
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+docker run -d \
+    -p 8000:8000 -p 9443:9443 \
+    --name portainer \
+    --restart=always \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v portainer_data:/data \
+    --label traefik.enable=true \
+    --label traefik.http.routers.portainer.rule=Host\(`portainer.deploypi`\) \
+    --label traefik.http.routers.portainer.entrypoints=websecure \
+    --label traefik.http.routers.portainer.tls=true \
+    --label traefik.http.services.portainer.loadbalancer.server.port=8000 \
+    portainer/portainer-ce:latest
