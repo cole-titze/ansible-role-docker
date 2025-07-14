0879b64ab93de7eecf1a17f06cabd61de5e57a69
Cole Titze <>
2025-07-13 21:49:25 -0500

    Update HA

--- files/containers/home-assistant.sh
+++ files/containers/home-assistant.sh
@@ -2,4 +2,15 @@
 docker stop -t 600 home-assistant
 docker rm home-assistant
 docker pull ghcr.io/home-assistant/home-assistant:stable
-docker run -d --name home-assistant --restart=always --device /dev/ttyUSB0:/dev/ttyUSB0 -v /var/homeassistant:/config --network host homeassistant/raspberrypi4-homeassistant
+docker run -d \
+  --name home-assistant \
+  --restart=always \
+  --device /dev/ttyUSB0:/dev/ttyUSB0 \
+  -v /var/homeassistant:/config \
+  --network host \
+  --label traefik.enable=true \
+  --label traefik.http.routers.homeassistant.rule=Host\(`homeassistant.deploypi`\) \
+  --label traefik.http.routers.homeassistant.entrypoints=websecure \
+  --label traefik.http.routers.homeassistant.tls=true \
+  --label traefik.http.services.homeassistant.loadbalancer.server.port=8123 \
+  homeassistant/raspberrypi4-homeassistant
