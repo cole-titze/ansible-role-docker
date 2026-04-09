314280eb00d068c8b87c703ab5509845708d47fc
Cole Titze <cole.titze@outlook.com>
2026-04-08 21:40:27 -0500

    Add updates

--- files/containers/nhl-odds-backup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mkdir -p ~/Backups
-docker exec nhl-odds-database-1 pg_dump -U postgres -Fc nhl > ~/Backups/nhl-$(date +%Y%m%d).dump 2>> /var/log/nhl-odds-backup.log
-find ~/Backups -name "nhl-*.dump" -mtime +7 -delete
